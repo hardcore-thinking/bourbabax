4fa8512f4b321f8cf148b9e508cd0d42fec35e7a
Maxiwere45 <amdjad9857@gmail.com>
2024-11-05 15:43:21 +0100

    🔄 Modify script-db.sql to drop existing `raspberries` table and ensure `serverdb` is used for new table creation

--- Server/database/script-db.sql
+++ Server/database/script-db.sql
@@ -1,9 +1,13 @@
 -- Table structure for table `raspberries`
+use serverdb;
+DROP TABLE IF EXISTS `raspberries`;
 CREATE TABLE IF NOT EXISTS `raspberries` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `mac_addr` VARCHAR(999) NOT NULL,
     `port` INT NOT NULL,
     `last_seen` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `ssh_key` VARCHAR(255) NOT NULL,
-    PRIMARY KEY (`id`),
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+COMMIT;
