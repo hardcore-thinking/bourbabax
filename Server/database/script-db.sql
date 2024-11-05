e7c3e5be4ad5fe654a89c53a7c0728e625a0abf9
Maxiwere45 <amdjad9857@gmail.com>
2024-11-05 15:29:15 +0100

    📦 Add initial SQL script to create `raspberries` table in the database with relevant fields and primary key configuration

---
+++ Server/database/script-db.sql
@@ -0,0 +1,9 @@
+-- Table structure for table `raspberries`
+CREATE TABLE IF NOT EXISTS `raspberries` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `mac_addr` VARCHAR(999) NOT NULL,
+    `port` INT NOT NULL,
+    `last_seen` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `ssh_key` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
