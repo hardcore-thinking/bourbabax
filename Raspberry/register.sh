96bba3701ea998aa8353aaf8beeefb09ec9d5101
Echalaye <etienne.chalaye@gmail.com>
2024-11-06 12:03:00 +0100

    raspeberry docker file work

--- Raspberry/register.sh
+++ Raspberry/register.sh
@@ -15,10 +15,10 @@ echo "Local ports: $LOCAL_PORTS"
 IFS=',' read -ra LOCAL_PORTS <<< "$LOCAL_PORTS"
 
 # Get ports from API with the user agent and the mac address
-# REMOTE_PORTS=$(curl -X POST -d "mac=$MAC&ports=$LOCAL_PORTS" -A "AirNet/1.0" $API_REGISTER_ENDPOINT)
+REMOTE_PORTS=$(curl -X POST -d "mac=$MAC&ports=$LOCAL_PORTS" -A "AirNet/1.0" $API_REGISTER_ENDPOINT)
 
 echo "Getting ports from API: $API_REGISTER_ENDPOINT, mac=$MAC, ports=$LOCAL_PORTS"
-REMOTE_PORTS=8022,8080,8443,18327
+# REMOTE_PORTS=8022,8080,8443,18327
 echo "Remote ports: $REMOTE_PORTS"
 # create an array of ports
 IFS=',' read -ra REMOTE_PORTS <<< "$REMOTE_PORTS"
@@ -31,6 +31,6 @@ for i in "${!LOCAL_PORTS[@]}"; do
     if lsof -Pi :$LOCAL_PORT -sTCP:LISTEN -t >/dev/null ; then
         echo "Local port $LOCAL_PORT is used"
         echo "Creating reverse SSH tunnel for port $LOCAL_PORT to $REMOTE_PORT"
-        # autossh -M 0 -f -N -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -R $REMOTE_PORT:localhost:$LOCAL_PORT $USER@$HOST
+        autossh -M 0 -f -N -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -R $REMOTE_PORT:localhost:$LOCAL_PORT $USER@$HOST
     fi
 done
