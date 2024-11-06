9e6783db59b26e3f8a7ae878e597cfb9d092c6ee
Echalaye <etienne.chalaye@gmail.com>
2024-11-06 16:27:41 +0100

    still alive

--- Raspberry/still_alive.sh
+++ Raspberry/still_alive.sh
@@ -3,6 +3,8 @@ USER="register"
 API_REGISTER_ENDPOINT="http://$HOST/api/ping"
 MAC=$(cat /sys/class/net/eth0/address)
 
-echo "test"
-# curl -X POST -d "mac=$MAC&ssh_key=$ssh_key" -A "AirNet/1.0" $API_REGISTER_ENDPOINT
-
+while true
+do
+    echo "test"
+    # curl -X POST -d "mac=$MAC&ssh_key=$ssh_key" -A "AirNet/1.0" $API_REGISTER_ENDPOINT
+done
