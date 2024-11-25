d5177bf1662fcfcf4086451b17f1ebbde3cd7839
ajvp <alexymansuy31@gmail.com>
2024-11-25 15:56:18 +0100

    API safe commit

--- Server/api/routes/api.php
+++ Server/api/routes/api.php
@@ -3,8 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/register', function (Request $request) {
-    return "Registering";
+Route::post('/register', function (Request $request) {
+
 });
 
 Route::get('/ping/{mac}', function (Request $request, string $mac) {
@@ -13,12 +13,4 @@ Route::get('/ping/{mac}', function (Request $request, string $mac) {
 
 Route::get('/manage-ports', function (Request $request) {
     return 'Managing ports';
-});
-
-Route::get('/test', function (Request $request) {
-    return response()->json([
-        ['id' => 1, 'name' => 'Produit 1', 'price' => 10.99],
-        ['id' => 2, 'name' => 'Produit 2', 'price' => 20.99],
-        ['id' => 3, 'name' => 'Produit 3', 'price' => 30.99],
-    ]);
 });
