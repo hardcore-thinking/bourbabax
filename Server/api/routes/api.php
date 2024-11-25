0002980d3b078f92d6314fae38139c10f7523224
ajvp <alexymansuy31@gmail.com>
2024-11-25 14:59:38 +0100

    Fixed private-airnet Dockerfile

--- Server/api/routes/api.php
+++ Server/api/routes/api.php
@@ -3,6 +3,22 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/register', function (Request $request) {
+    return "Registering";
+});
+
+Route::get('/ping/{mac}', function (Request $request, string $mac) {
+    return shell_exec("ping {$mac}");
+});
+
+Route::get('/manage-ports', function (Request $request) {
+    return 'Managing ports';
+});
+
 Route::get('/test', function (Request $request) {
-    return "test";
+    return response()->json([
+        ['id' => 1, 'name' => 'Produit 1', 'price' => 10.99],
+        ['id' => 2, 'name' => 'Produit 2', 'price' => 20.99],
+        ['id' => 3, 'name' => 'Produit 3', 'price' => 30.99],
+    ]);
 });
