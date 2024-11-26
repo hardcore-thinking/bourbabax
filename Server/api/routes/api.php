b3b7e8583a49229a31c3d46e1bda87da423d2913
ajvp <alexymansuy31@gmail.com>
2024-11-26 14:31:54 +0100

    Updated docker-compose.yaml

--- Server/api/routes/api.php
+++ Server/api/routes/api.php
@@ -10,7 +10,7 @@ Route::get('/register/{mac}', function (Request $request, string $mac) {
 });
 
 Route::get('/heartbeat', function (Request $request, string $mac) {
-    return "Heartbeat from {$mac}";  
+    return "Heartbeat from {$mac}";
 });
 
 Route::get('/manage-ports', function (Request $request) {
